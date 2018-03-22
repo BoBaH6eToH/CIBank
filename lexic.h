11ed5d7d54275785d9faa9a48a793e9d76830e7e
Vladimir Chikishev <freezedunk@gmail.com>
2018-03-22 13:09:46 +0500

    Commented

--- lexic.h
+++ lexic.h
@@ -1,4 +1,18 @@
 #include <string>
 
+/**
+* getTripleProp(int value, int genderID, int caseID)
+* функция используется для преобразования трехзначного числа в строку независимо от разряда
+* 
+* @param value    исходное значение
+* @param genderID ID рода в диапазоне [0;2]
+* @param caseID   ID падежа в диапазоне [0;5]
+* @return result  буквенное написание value в соответствии с родом и падежом
+*/
 std::string getTripleProp(int value, int genderID, int caseID);
+
+/**
+* std::string sumProp(long long nSum, std::string sGender, std::string sCase)
+* объявление функции по ТЗ
+*/
 std::string sumProp(long long nSum, std::string sGender, std::string sCase);
